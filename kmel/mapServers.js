fec48b9f1810223862a1db778e7da8df1d39b68c
mengan <75980861+Oliver-Assenmacher@users.noreply.github.com>
2022-02-03 12:06:11 +0100

    Update mapServers.js

--- kmel/mapServers.js
+++ kmel/mapServers.js
@@ -1,24 +1,24 @@
 export async function main(ns) {
     var servers = ["home"];
-    ns.clear("servers.txt");
+    await ns.clear("servers.txt");
     for (let i = 0; i < servers.length; ++i) {
         var hostname = servers[i];
-        ns.print(hostname + " logged.");
-        ns.write("servers.txt", hostname +
-            "," + ns.getServerRam(hostname)[0] +
-            "," + ns.getServerNumPortsRequired(hostname) +
-            "," + ns.getServerRequiredHackingLevel(hostname) +
-            "," + ns.getServerMaxMoney(hostname) +
-            "," + ns.getServerMinSecurityLevel(hostname) +
-            "," + ns.getServerGrowth(hostname) +
+        await ns.print(hostname + " logged.");
+        await ns.write("servers.txt", hostname +
+            "," + await ns.getServerRam(hostname)[0] +
+            "," + await ns.getServerNumPortsRequired(hostname) +
+            "," + await ns.getServerRequiredHackingLevel(hostname) +
+            "," + await ns.getServerMaxMoney(hostname) +
+            "," + await ns.getServerMinSecurityLevel(hostname) +
+            "," + await ns.getServerGrowth(hostname) +
             "\r\n");
 
-        var newScan = ns.scan(hostname);
+        var newScan = await ns.scan(hostname);
         for (let j = 0; j < newScan.length; j++) {
             if (servers.indexOf(newScan[j]) == -1) {
                 servers.push(newScan[j]);
             }
         }
     }
-    ns.tprint("Network mapped with " + servers.length + " servers.");
+    await ns.tprint("Network mapped with " + servers.length + " servers.");
 }
